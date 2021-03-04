28f767a5c96a68fbd84288778c0e7911798d14a1
Jonathan Metzman <metzman@chromium.org>
2021-03-03 18:17:24 -0800

    Add race

--- do_stuff_fuzzer.cpp
+++ do_stuff_fuzzer.cpp
@@ -4,6 +4,13 @@
 
 #include <string>
 #include <stdlib.h>
+#include <pthread.h>
+
+int Global;
+void *Thread1(void *x) {
+  Global = 42;
+  return x;
+}
 
 // Simple fuzz target for DoStuff().
 // See http://libfuzzer.info for details.
@@ -11,8 +18,11 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   std::string str(reinterpret_cast<const char *>(data), size);
   DoStuff(str);  // Disregard the output.
   if (data[0] == 'a') {
-    int* x = (int*) malloc(4);
-    free(x);
-    return x[0];
+    pthread_t t;
+    pthread_create(&t, NULL, Thread1, NULL);
+    Global = 43;
+    pthread_join(t, NULL);
+    return 0;
   }
+  return 0;
 }
