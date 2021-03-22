e2a270e427d21c3eecd0e02cd61365752106bc2f
Jonathan Metzman <metzman@chromium.org>
2021-03-22 08:24:09 -0700

    Test cifuzz-processes

---
+++ no_crash_fuzzer.cpp
@@ -0,0 +1,7 @@
+// Copyright 2017 Google Inc. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+#include <stddef.h>
+#include <stdint.h>
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+  return 0;
+}
